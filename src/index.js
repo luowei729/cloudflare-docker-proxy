b3decca60567e3b95acd5dc4e897751c2453aaa3
liusanxiang <1084605775@qq.com>
2024-09-30 18:28:26 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.lkz.pub": dockerHub,
+  "quay.lkz.pub": "https://quay.io",
+  "gcr.lkz.pub": "https://gcr.io",
+  "k8s-gcr.lkz.pub": "https://k8s.gcr.io",
+  "k8s.lkz.pub": "https://registry.k8s.io",
+  "ghcr.lkz.pub": "https://ghcr.io",
+  "cloudsmith.lkz.pub": "https://docker.cloudsmith.io",
+  "ecr.lkz.pub": "https://public.ecr.aws",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
